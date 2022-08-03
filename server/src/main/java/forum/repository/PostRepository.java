f23269653edaf34144ce1b63fec4cf37c7bb168e
AdamZych333 <adam.zych99@wp.pl>
2022-08-03 17:40:04 +0200

    added post repository

---
+++ server/src/main/java/forum/repository/PostRepository.java
@@ -0,0 +1,7 @@
+package forum.repository;
+
+import forum.entity.Post;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PostRepository extends JpaRepository<Post, Long>{
+}
