a56fb8d7b51d2d4d55c675f3af27a0cca233c66a
AdamZych333 <adam.zych99@wp.pl>
2022-08-21 13:09:38 +0200

    added http token interceptor

--- client/src/app/core/interceptors/http.token.interceptor.ts
+++ client/src/app/core/interceptors/http.token.interceptor.ts
@@ -1,11 +1,28 @@
 import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { Observable } from "rxjs";
+import { JwtService } from "../services";
 
 @Injectable()
 export class HttpTokenInterceptor implements HttpInterceptor {
-    intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-        throw new Error("Method not implemented.");
-    }
     
+    constructor(private jwtService: JwtService) {}
+
+    intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+        
+        const headersConfig: {[k: string]: string} = {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json',
+        }
+
+        const token = this.jwtService.getToken();
+
+        if(token){
+            headersConfig['Authorization'] = `Bearer ${token}`;
+        }
+
+        const request = req.clone({ setHeaders: headersConfig });
+
+        return next.handle(request);
+    }
 }
