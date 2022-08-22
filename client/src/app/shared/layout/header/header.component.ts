8c2fb1935e6799348fc577af8eef92a8ccc35fb3
AdamZych333 <adam.zych99@wp.pl>
2022-08-22 10:57:30 +0200

    added signing out

--- client/src/app/shared/layout/header/header.component.ts
+++ client/src/app/shared/layout/header/header.component.ts
@@ -1,9 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { map, Observable } from 'rxjs';
+import { AuthService } from 'src/app/core';
 
 @Component({
   selector: 'layout-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.sass']
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
+  isAuthenticated$ = new Observable<boolean>;
+
+  constructor(
+    private authService: AuthService
+  ){}
+
+  ngOnInit(): void {
+    this.isAuthenticated$ = this.authService.inAuthenticatedSubject.asObservable().pipe(
+      map(isAuth => !isAuth),
+    )
+  }
+
+  onSignOutClick(){
+    this.authService.removeAuth();
+  }
 }
